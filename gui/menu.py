6a3f547925193aeefb8750ce1e7e271d79a6379d
François Pietka <francois@pietka.fr>
2014-01-11 16:35:14 +0100

    comment group

--- gui/menu.py
+++ gui/menu.py
@@ -9,46 +9,55 @@ class ApplicationMenu(QtGui.QMenuBar):
     def __init__(self, window):
         super(ApplicationMenu, self).__init__(window)
 
-        #exit action
+        #{{{ exit action
         exitAction = QtGui.QAction(QtGui.QIcon(consts.exitIcon), '&Exit', window)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(QtGui.qApp.quit)
-        #new tab action
+        #}}}
+        #{{{ new tab action
         newTabAction = QtGui.QAction(QtGui.QIcon(), '&New', window)
         newTabAction.setStatusTip('Create new tab')
         newTabAction.setShortcut('Ctrl+N')
         newTabAction.triggered.connect(window._addNewTab)
-        #save current tab action
+        #}}}
+        #{{{ save current tab action
         saveCurrentTabAction = QtGui.QAction(QtGui.QIcon(), '&Save current', window)
         saveCurrentTabAction.setStatusTip('Save current tab to CSV')
         saveCurrentTabAction.setShortcut('Ctrl+S')
         saveCurrentTabAction.triggered.connect(window._saveTab)
-        #delete current tab action
+        #}}}
+        #{{{ delete current tab action
         deleteCurrentTabAction = QtGui.QAction(QtGui.QIcon(), '&Delete current', window)
         deleteCurrentTabAction.setStatusTip('Delete current tab')
         deleteCurrentTabAction.triggered.connect(window._deleteTab)
         deleteCurrentTabAction.setShortcut('Ctrl+W')
-        #purge ticket action
+        #}}}
+        #{{{ purge ticket action
         purgeTicketsAction = QtGui.QAction(QtGui.QIcon(consts.purgeIcon), '&Purge current tab', window)
         purgeTicketsAction.setStatusTip('Delete all tickets')
         purgeTicketsAction.triggered.connect(window._purgeTickets)
-        #purge tabs action
+        #}}}
+        #{{{ purge tabs action
         purgeTabsAction = QtGui.QAction(QtGui.QIcon(consts.purgeIcon), '&Remove all tabs', window)
         purgeTabsAction.setStatusTip('Delete all tabs')
         purgeTabsAction.triggered.connect(window._purgeTabs)
-        #settings window action
+        #}}}
+        #{{{ settings window action
         showSettingsAction = QtGui.QAction(QtGui.QIcon(consts.settingsIcon), 'Se&ttings', window)
         showSettingsAction.setStatusTip('Open settings window')
         showSettingsAction.triggered.connect(window._showSettings)
-        # color settings window action
+        #}}}
+        #{{{ color settings window action
         showColorSettingsAction = QtGui.QAction(QtGui.QIcon(consts.colorSettingsIcon), '&Color settings', window)
         showColorSettingsAction.setStatusTip('Open color settings window')
         showColorSettingsAction.triggered.connect(window._showColorSettings)
-        #about window action
+        #}}}
+        #{{{ about window action
         showAboutAction = QtGui.QAction(QtGui.QIcon(), '&About', window)
         showAboutAction.setStatusTip('Open about window')
         showAboutAction.triggered.connect(window._showAbout)
+        #}}}
 
         appMenu = self.addMenu('&Application')
         appMenu.addAction(showSettingsAction)
